90b5c0f8bd45a6dbe1445ca764a9007101a84dd5
Mad1ax <62326559+Mad1ax@users.noreply.github.com>
2021-09-01 20:58:36 +0700

    Update users.jsx

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react'
-import api from "../API";
+import api from "../API"
 
 const Users = () => {
 
@@ -25,41 +25,24 @@ const Users = () => {
             <thead>
             <tr>
                 <th scope="col">Имя</th>
-                <th scope="col">Качество</th>
+                <th scope="col">Качества</th>
                 <th scope="col">Профессия</th>
                 <th scope="col">Встретился, раз</th>
-                <th scope="col">Оцека</th>
+                <th scope="col">оценка</th>
             </tr>
             </thead>
             <tbody>
             {users.map((user) => {
                 return (
-                    <tr key={user._id}>
-                        <td>{user.name}</td>
-                        <td>
-                            {user.qualities.map((item) => (
-                                <span
-                                    className={
-                                        "badge m-1 bg-" + item.color
-                                    }
-                                    key={item._id}
-                                >
-                                            {item.name}
-                                        </span>
-                            ))}
-                        </td>
-                        <td>{user.profession.name}</td>
-                        <td>{user.completedMeetings}</td>
-                        <td>{user.rate}</td>
-                        <td>
-                            <button
-                                className={"btn btn-danger"}
-                                onClick={() => handleDelete(user._id)}
-                            >
-                                Удалить
-                            </button>
-                        </td>
-                    </tr>
+                <tr key={user._id}>
+                    <td>{user.name}</td>
+                    <td>{user.qualities.map((item) => (
+                        <span className={"badge m-1 bg-" + item.color} key={item._id}>{item.name}</span>))}</td>
+                    <td>{user.profession.name}</td>
+                    <td>{user.completedMeetings}</td>
+                    <td>{user.rate}</td>
+                    <td><button className={"btn btn-danger"} onClick={() => handleDelete(user._id)}>delete</button></td>
+                </tr>
                 )
             })}
             </tbody>
@@ -68,5 +51,3 @@ const Users = () => {
 }
 
 export default Users
-
-
